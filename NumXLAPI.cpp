dc6be5444558a8dc92de23b17adc774ef55130e5
melbawab <mohamad@spiderfinancial.com>
2015-01-28 11:53:31 -0600

    Added a simple function for testing

--- NumXLAPI.cpp
+++ NumXLAPI.cpp
@@ -23,7 +23,32 @@
 
 // NumXLAPI.cpp : Defines the exported functions for the DLL application.
 //
-
 #include "stdafx.h"
 
+/*
+* Function to be called from Python
+*/
+extern "C" static PyObject* py_myFunction(PyObject* self, PyObject* args)
+{
+  char *s = "Hello from C!";
+  return Py_BuildValue("s", s);
+}
+
+
+/*
+* Bind Python function names to our C functions
+*/
+static PyMethodDef myModule_methods[] = {
+    { "myFunction", py_myFunction, METH_VARARGS },
+    { NULL, NULL }
+};
+
+
+/*
+* Python calls this to let us initialize our module
+*/
+extern "C"  void initmyModule()
+{
+  (void)Py_InitModule("myModule", myModule_methods);
+}
 
--- stdafx.h
+++ stdafx.h
@@ -27,7 +27,10 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
 #include <windows.h>
+#include <cmath>
+
 
 
 
 // TODO: reference additional headers your program requires here
+#include <Python.h>
